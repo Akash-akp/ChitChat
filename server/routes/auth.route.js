1dd1066f3e7e53428774a47594238ab625d95a15
Akash-akp <akashakp0037@gmail.com>
2024-12-29 13:18:52 +0530

    update

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { signup, signin } = require('../controllers/auth.controller');
+const authController  = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.post('/signup', signup);  
-router.post('/signin', signin);
+router.post('/signup', authController.signup);  
+router.post('/signin', authController.signin);
 
 
 module.exports = router;
